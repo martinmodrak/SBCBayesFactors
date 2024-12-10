a76ac050ba66836c46715887861e4616c9c3af95
martinmodrak <modrak.mar@gmail.com>
2024-12-10 13:23:57 +0000

    Turtles - ChiSq

--- R/cluster.R
+++ R/cluster.R
@@ -1,5 +1,5 @@
 setup_cluster <- function() {
-  mirai::daemons(parallelly::availableCores(), dispatcher = FALSE, force = FALSE)
+  mirai::daemons(min(parallelly::availableCores(), 40), dispatcher = FALSE, force = FALSE)
   future::plan(future.mirai::mirai_cluster)
   options(SBC.generator_chunk_size = 500)
 }
