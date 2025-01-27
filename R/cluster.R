641b7e80495f7171531b90292f8439733c1a9630
Martin Modrák <modrak.mar@gmail.com>
2025-01-27 15:03:28 +0100

    Small progress on paper

--- R/cluster.R
+++ R/cluster.R
@@ -1,5 +1,11 @@
-setup_cluster <- function() {
-  mirai::daemons(min(parallelly::availableCores(), 40), dispatcher = FALSE, force = FALSE)
+setup_cluster <- function(max_local_workers = Inf, max_server_workers = 40) {
+  is_local <- parallelly::availableCores() < 40
+  if(is_local) {
+    n_workers <- min(parallelly::availableCores(), max_local_workers)
+  } else {
+    n_workers <- min(parallelly::availableCores(), max_server_workers)
+  }
+  mirai::daemons(n_workers, dispatcher = FALSE, force = FALSE)
   future::plan(future.mirai::mirai_cluster)
   options(SBC.generator_chunk_size = 500)
 }
