154b8ce7925733a2c8a8c5f87c8e48c881bfe621
Martin Modrák <modrak.mar@gmail.com>
2025-02-10 16:50:13 +0100

    Good check + examples + math

--- R/plots.R
+++ R/plots.R
@@ -1,3 +1,12 @@
-my_reliability_diag <- function(binary_probs) {
-  autoplot(reliabilitydiag::reliabilitydiag(x = binary_probs$prob, y = binary_probs$simulated_value, region.level = 0.95, region.method = "resampling", n.boot = 1000))
+my_reliability_diag <- function(binary_probs, region.method = "resampling") {
+  autoplot(
+    reliabilitydiag::reliabilitydiag(
+      x = binary_probs$prob,
+      y = binary_probs$simulated_value,
+      region.level = 0.95,
+      region.method = region.method,
+      n.boot = 1000
+    ),
+    params_CEPsegment = list(linewidth = 0.5, color = "red")
+  )
 }
